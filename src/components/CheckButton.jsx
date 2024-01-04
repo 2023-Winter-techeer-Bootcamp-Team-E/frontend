8bad9bdea692257ae77c10a9adb12cec5d6334b5
jung2941 <eugene2941@gmail.com>
2024-01-05 03:30:16 +0900

    design/#24

---
+++ src/components/CheckButton.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+function CheckButton({ closeModal, isPasswordValid }) {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const CheckButtonClick = () => {
+    if (isPasswordValid) {
+      console.log('CheckButton clicked!');
+      closeModal();
+
+    } else {
+      console.log('Invalid Password');
+    }
+  };
+
+  return (
+    <CheckButtonContainer
+      onClick={CheckButtonClick}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      isHovered={isHovered}
+      isPasswordValid={isPasswordValid}>
+      확인
+    </CheckButtonContainer>
+  );
+}
+
+//확인 버튼
+const CheckButtonContainer = styled.div`
+  position: absolute;
+  width: 8.325rem;
+  height: 4.3025rem;
+  border-radius: 5rem;
+  background: ${(props) => (props.isHovered ? '#B1B3FF' : '#C1C3FF')};
+  cursor: ${(props) => (props.isPasswordValid ? 'pointer' : 'not-allowed')};
+  cursor: pointer;
+  left: 31.75rem;
+  top: 15.5rem;
+  z-index: 5;
+  box-shadow: 0px 3px 3px 0px rgba(0, 0, 0, 0.15);
+  transition: all 0.3s ease-in-out;
+
+  // 글자 */
+  color: #fff;
+  font-family: 'Noto Sans';
+  font-size: 1.45rem;
+  font-weight: 800;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  &:hover {
+    transform: scale(1.05);
+    box-shadow: 0px 5px 5px 0px rgba(0, 0, 0, 0.25);
+  }
+`;
+export default CheckButton;
+
+
---
+++ src/components/GuestModal.jsx
@@ -0,0 +1,98 @@
+// 게스트 모달 컴포넌트
+import { useState } from 'react';
+import styled from 'styled-components';
+import CheckButton from '../components/CheckButton';
+import GuestPwInput from '../components/GuestPwInput';
+
+function GuestModal({ closeModal }) {
+  const [password, setPassword] = useState('');
+  const [isPasswordValid, setIsPasswordValid] = useState(false);
+
+  const handlePasswordChange = (newPassword) => {
+    setPassword(newPassword);
+    setIsPasswordValid(newPassword.length === 4 && /^\d+$/.test(newPassword));
+  };
+
+  return (
+    <ModalWrapper>
+      <ModalContent>
+        <PwMessage>비밀번호 4자리를 설정해주세요!</PwMessage>
+        <GuestPwInput
+          type="password"
+          placeholder="비밀번호"
+          text={password}
+          handleTextChange={handlePasswordChange}
+        />
+        <StyledCloud2 src="/img/Cloud2.png" alt="Cloud 2" />
+        <StyledLogo_Shadow src="/img/Logo_Shadow.png" alt="Logo_Shadow" />
+        <CheckButton closeModal={closeModal} isPasswordValid={isPasswordValid} />
+      </ModalContent>
+    </ModalWrapper>
+  );
+}
+
+// 모달 배경
+const ModalWrapper = styled.div`
+  position: fixed;
+  z-index: 1;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgba(0, 0, 0, 0.4);
+  /* display: ${(props) => (props.show ? 'block' : 'none')}; */
+  backdrop-filter: blur(3px); // 뒷 배경 블러 처리
+`;
+// 모달 창
+const ModalContent = styled.div`
+  background-color: #F8F8F8;
+  width: 49.0625rem;
+  height: 44.37006rem;
+  flex-shrink: 0;
+  border-radius: 1.4375rem;
+  display: flex;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  align-items: center;
+  justify-content: flex-start;
+  position: relative;
+`;
+
+
+//비밀번호 설정해주세요 텍스트
+const PwMessage = styled.div`
+  color: #2C2C2C;
+  font-family: 'BMJUA';
+  font-size: 1.625rem;
+  // font-style: normal;
+  // font-weight: 400;
+  // line-height: normal;
+  position: absolute;
+  top: 9.8rem;
+  left: 8.15rem;
+`;
+
+//구름 이미지
+const StyledCloud2 = styled.img`
+  position: absolute;
+  width: 10.5625rem;
+  height: 8.4375rem;
+  z-index: 3;
+  top: 7.43rem;
+  left: 30.62rem;
+`;
+
+// 로고 이미지
+const StyledLogo_Shadow = styled.img`
+  position: absolute;
+  width: 24.1875rem;
+  height: 19.3rem;
+  flex-shrink: 0;
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+  z-index: 10;
+  top: 21.44rem;
+  left: 12.06rem;
+`;
+export default GuestModal;
