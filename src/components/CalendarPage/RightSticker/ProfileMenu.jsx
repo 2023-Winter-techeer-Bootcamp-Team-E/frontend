7fce041731b56b294fa77b8c3e8c98be7bbca01d
j._.jinoo <jinoo0306@naver.com>
2024-01-06 01:20:06 +0900

    design : 프로필 메뉴 디자인 완성

--- src/components/CalendarPage/RightSticker/ProfileMenu.jsx
+++ src/components/CalendarPage/RightSticker/ProfileMenu.jsx
@@ -13,10 +13,10 @@ function ProfileMenu(props) {
           <rect y="30.0996" width="228" height="54.825" fill="#C6F1FF" />
         </svg>
       </ProfileMenuTop>
-      <UserId />
-      <UserName />
-      <HaruConnectingHelp />
-      <LogOut />
+      <UserId>UserIdNull</UserId>
+      <UserName>UserNameNull</UserName>
+      <HaruConnectingHelp>하루 연결 도움말</HaruConnectingHelp>
+      <LogOut>로그아웃</LogOut>
     </ProfileMenuFrame>
   );
 }
@@ -29,6 +29,17 @@ const ProfileMenuFrame = styled.div`
   border-radius: 1.25rem;
   background: #e8f7fc;
   box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  display: flex;
+  justify-content: center;
+`;
+
+// ProfileMenuTop에 대한 스타일 적용
+const ProfileMenuTop = styled.div`
+  position: absolute;
+  width: 14.25rem;
+  height: 5.375rem;
+  flex-shrink: 0;
+  z-index: 1;
   svg {
     width: 100%;
     height: 100%;
@@ -36,31 +47,72 @@ const ProfileMenuFrame = styled.div`
   }
 `;
 
-// ProfileMenuTop에 대한 스타일 적용
-const ProfileMenuTop = styled.div`
-  width: 14.25rem;
-  height: 5.375rem;
-  flex-shrink: 0;
-`;
-
 // UserId에 대한 스타일 적용
 const UserId = styled.div`
-  /* 여기에 UserId에 대한 스타일을 추가하세요 */
+  position: absolute;
+  margin-top: 1.13rem;
+  left: 1.06rem;
+  z-index: 2;
+
+  color: #aaa;
+  text-align: center;
+  font-family: Arial Black;
+  font-size: 1rem;
+  font-style: normal;
+  font-weight: 900;
+  line-height: normal;
 `;
 
 // UserName에 대한 스타일 적용
 const UserName = styled.div`
-  /* 여기에 UserName에 대한 스타일을 추가하세요 */
+  position: absolute;
+  margin-top: 3.06rem;
+  left: 1.06rem;
+  z-index: 2;
+
+  color: #aaa;
+  text-align: center;
+  font-family: Arial Black;
+  font-size: 1rem;
+  font-style: normal;
+  font-weight: 900;
+  line-height: normal;
 `;
 
 // HaruConnectingHelp에 대한 스타일 적용
 const HaruConnectingHelp = styled.div`
-  /* 여기에 HaruConnectingHelp에 대한 스타일을 추가하세요 */
+  position: absolute;
+  margin-top: 6.94rem;
+  left: 1.06rem;
+  z-index: 2;
+
+  color: #aaa;
+  text-align: center;
+  font-family: Arial Black;
+  font-size: 1rem;
+  font-style: normal;
+  font-weight: 900;
+  line-height: normal;
+  cursor: pointer;
+  text-decoration: none;
 `;
 
 // LogOut에 대한 스타일 적용
 const LogOut = styled.div`
-  /* 여기에 LogOut에 대한 스타일을 추가하세요 */
+  position: absolute;
+  margin-top: 9.88rem;
+  left: 1.06rem;
+  z-index: 2;
+
+  color: #dd0000;
+  text-align: center;
+  font-family: Arial Black;
+  font-size: 1rem;
+  font-style: normal;
+  font-weight: 900;
+  line-height: normal;
+  cursor: pointer;
+  text-decoration: none;
 `;
 
 export default ProfileMenu;
