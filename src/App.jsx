2e141d84b0e8399848458afd0caea8c3de1c473c
j._.jinoo <jinoo0306@naver.com>
2024-01-06 00:44:46 +0900

    design : Profile Menu 제작 임시저장

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import CalendarPage from './pages/CalendarPage';
 import StartPage from './pages/StartPage';
 import LoginPage from './pages/LoginPage';
 import SignUpPage from './pages/SignUpPage';
+import ProfileMenu from './components/CalendarPage/RightSticker/ProfileMenu';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/calendar" element={<CalendarPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/menu" element={<ProfileMenu />} />
       </Routes>
     </Router>
   );
--- src/components/CalendarPage/RightSticker/ProfileMenu.jsx
+++ src/components/CalendarPage/RightSticker/ProfileMenu.jsx
@@ -4,7 +4,15 @@ import styled from 'styled-components';
 function ProfileMenu(props) {
   return (
     <ProfileMenuFrame>
-      <ProfileMenuTop />
+      <ProfileMenuTop>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 228 86"
+          fill="#C6F1FF">
+          <rect width="228" height="86" rx="20" fill="#C6F1FF" />
+          <rect y="30.0996" width="228" height="54.825" fill="#C6F1FF" />
+        </svg>
+      </ProfileMenuTop>
       <UserId />
       <UserName />
       <HaruConnectingHelp />
@@ -15,12 +23,24 @@ function ProfileMenu(props) {
 
 // ProfileMenuFrame에 대한 스타일 적용
 const ProfileMenuFrame = styled.div`
-  /* 여기에 ProfileMenuFrame에 대한 스타일을 추가하세요 */
+  width: 14.25rem;
+  height: 15.625rem;
+  flex-shrink: 0;
+  border-radius: 1.25rem;
+  background: #e8f7fc;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  svg {
+    width: 100%;
+    height: 100%;
+    fill: none;
+  }
 `;
 
 // ProfileMenuTop에 대한 스타일 적용
 const ProfileMenuTop = styled.div`
-  /* 여기에 ProfileMenuTop에 대한 스타일을 추가하세요 */
+  width: 14.25rem;
+  height: 5.375rem;
+  flex-shrink: 0;
 `;
 
 // UserId에 대한 스타일 적용
