06140378f57292f26e23cdc936c58642ddb0a51a
j._.jinoo <jinoo0306@naver.com>
2024-01-21 22:26:55 +0900

    design : 필요없는 라이브러리 제거

--- src/components/LoginInput.jsx
+++ src/components/LoginInput.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 
 const LoginInput = ({ text, handleTextChange, placeholder, type }) => {
