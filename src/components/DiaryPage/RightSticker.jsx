1b27a1c5243620ad5f72caa6a54c3098c01253d1
j._.jinoo <jinoo0306@naver.com>
2024-01-25 17:58:12 +0900

    feat : dalle 이미지 api호출 완료

--- src/components/DiaryPage/RightSticker.jsx
+++ src/components/DiaryPage/RightSticker.jsx
@@ -8,14 +8,49 @@ const RightSticker = () => {
   const [stickerImages, setStickerImages] = useState([]);
 
   const handleStickerClick = (image) => {
-    // 여기서 onStickerSelect는 상위 컴포넌트로부터 전달받은 함수입니다.
     onStickerSelect(image);
   };
+
+  useEffect(() => {
+    const fetchStickerImages = async () => {
+      try {
+        // content 값을 생성
+        const content =
+          '나는 맥도날드에서 코카콜라를 먹었다. 콜라랑 감자튀김도 추가해서 세트로 먹었다. 참 맛있었다.'; // 실제로 사용할 content 값을 넣어주세요
+        console.log('Dall-e 스티커 생성중...');
+
+        const response = await baseInstance.post('/diaries/stickers', {
+          content,
+        });
+
+        // const response = await baseInstance.get(`/static/stickers?page=1`);
+
+        const data = response.data.data;
+        // API에서 받아온 이미지 URL을 상태에 업데이트
+        setStickerImages(data.sticker_image_urls);
+        // setStickerImages(data.st_image_urls);
+
+        console.log('Dall-e 스티커 생성 완료!');
+      } catch (error) {
+        console.error('Dall-e 스티커 API 호출 에러! :', error);
+      }
+    };
+
+    // 컴포넌트가 마운트될 때 한 번 호출
+    fetchStickerImages();
+  }, []); // 빈 배열을 전달하여 컴포넌트가 마운트될 때만 호출
 
   return (
     <div>
       <RightStickerContainer>
-        {/* 작성 필요 */}
+        {stickerImages.map((image, index) => (
+          <DalleStickerBox
+            key={index}
+            onClick={() => handleStickerClick(image)}>
+            <DalleSticker src={image} alt={`Sticker ${index + 1}`} />
+          </DalleStickerBox>
+        ))}
+
         <StyledCloud1 src={Cloud1} alt="Cloud 1" />
         <StyledCloud2 src={Cloud2} alt="Cloud 2" />
       </RightStickerContainer>
@@ -33,12 +68,15 @@ const RightStickerContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(1, 1fr);
   grid-template-rows: repeat(2, 1fr);
-  align-items: stretch;
-  justify-content: center;
+  align-items: center; /* 수직 정렬 중앙 */
+  justify-content: center; /* 수평 정렬 중앙 */
+  display: flex;
+  flex-direction: column;
   position: relative;
   padding-top: 5rem;
   padding-bottom: 5rem;
 `;
+
 const DalleStickerBox = styled.div`
   align-items: center;
   justify-content: center;
