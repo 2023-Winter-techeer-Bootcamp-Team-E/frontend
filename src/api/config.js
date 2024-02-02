67d61727df9e9abb89d85431f7ca883492ce9954
woohee <aldkgha@naver.com>
2024-02-03 00:53:05 +0900

    feat: 최종저장 페이지

--- src/api/config.js
+++ src/api/config.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = '/api/v1'; // 배포용
-// const BASE_URL = 'http://localhost:8000/api/v1'; // 개발용
+// const BASE_URL = '/api/v1'; // 배포용
+const BASE_URL = 'http://localhost:8000/api/v1'; // 개발용
 export const baseInstance = axios.create({
   withCredentials: true,
   baseURL: BASE_URL, // 기본 URL 설정
